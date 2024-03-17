c119c0b86b775a92bd429dc996a164730ce70b55
Isaac Goh <isgoh@u81z05s04.42kl.edu.my>
2024-03-17 15:44:59 +0800

    make changes to rush04.c

--- rush04.c
+++ rush04.c
@@ -1,45 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   testrush04.c                                       :+:      :+:    :+:   */
+/*   rush04.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isgoh <isgoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 10:59:30 by isgoh             #+#    #+#             */
-/*   Updated: 2024/03/17 15:03:08 by isgoh            ###   ########.fr       */
+/*   Updated: 2024/03/17 15:44:19 by isgoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	rush(int xWidth, int yHeight)
+void	rush(int xW, int yH)
 {
-	int x = 0;
-	int y = 1;
-	while ((y <= yHeight && x <= xWidth))
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 1;
+	while ((y <= yH && x <= xW))
 	{
 		x++;
-		if (x >= xWidth + 1)
+		if (x >= xW + 1)
 		{
 			ft_putchar('\n');
 			x = 0;
 			y = y + 1;
 		}
-		else if ((x == 1 && y == 1) || (x >= xWidth && y >= yHeight && yHeight != 1))
-		{
+		else if ((x == 1 && y == 1) || (x >= xW && y >= yH && yH != 1))
 			ft_putchar('A');
-		}
-		else if ((x == xWidth && y == 1) || (x == 1 && y == yHeight))
-		{
+		else if ((x == xW && y == 1) || (x == 1 && y == yH))
 			ft_putchar('C');
-		}
-		else if (((x >= 2 || x == xWidth - 1) && (y == 1 || y == yHeight)) || ((x == 1 || x == xWidth) && (y >= 2 || y == yHeight - 1)))
-		{
+		else if ((x >= 2 || x == xW - 1) && (y == 1 || y == yH))
 			ft_putchar('B');
-		}
+		else if ((x == 1 || x == xW) && (y >= 2 || y == yH - 1))
+			ft_putchar('B');
 		else
-		{
 			ft_putchar(' ');
-		}
 	}
-}	
+}
