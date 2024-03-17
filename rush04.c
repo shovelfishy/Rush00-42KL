7897cc757ddaf6247c34dddd6407769d755ae4bc
Isaac Goh <isgoh@u81z05s04.42kl.edu.my>
2024-03-17 15:04:35 +0800

    make changes to rush04.c

--- rush04.c
+++ rush04.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush04.c                                           :+:      :+:    :+:   */
+/*   testrush04.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isgoh <isgoh@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 10:59:30 by isgoh             #+#    #+#             */
-/*   Updated: 2024/03/17 11:33:35 by isgoh            ###   ########.fr       */
+/*   Updated: 2024/03/17 15:03:08 by isgoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,55 +16,30 @@ void	rush(int xWidth, int yHeight)
 {
 	int x = 0;
 	int y = 1;
-	while (!(y >= yHeight + 1));
+	while ((y <= yHeight && x <= xWidth))
 	{
-		xCount++;
+		x++;
 		if (x >= xWidth + 1)
 		{
 			ft_putchar('\n');
-			x = 1;
+			x = 0;
 			y = y + 1;
 		}
-		if (y == 1)
+		else if ((x == 1 && y == 1) || (x >= xWidth && y >= yHeight && yHeight != 1))
 		{
-			if (x == 1)
-			{
-				ft_putchar('A');
-			}
-			else if (x == xWidth)
-			{
-				ft_putchar('C');
-			}
-			else
-			{
-				ft_putchar('B');
-			}
+			ft_putchar('A');
 		}
-		else if (y == yHeight)
+		else if ((x == xWidth && y == 1) || (x == 1 && y == yHeight))
 		{
-			if (x == 1)
-			{
-				ft_putchar('C');
-			}
-			else if (x == xWidth)
-			{
-				ft_putchar('A');
-			}
-			else
-			{
-				ft_putchar('B');
-			}
+			ft_putchar('C');
 		}
-		else if (y >= 2 && y <= yHeight - 1)
+		else if (((x >= 2 || x == xWidth - 1) && (y == 1 || y == yHeight)) || ((x == 1 || x == xWidth) && (y >= 2 || y == yHeight - 1)))
 		{
-			if (x == 1 || x == xWidth)
-			{
-				ft_putchar('B');
-			}
-			else
-			{
-				ft_putchar(' ');
-			}
+			ft_putchar('B');
+		}
+		else
+		{
+			ft_putchar(' ');
 		}
 	}
-}
+}	
