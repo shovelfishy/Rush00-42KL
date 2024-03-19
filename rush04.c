1e68f631229314e8457b54dc59efad09c17856ba
afdzal <163637039+afdzal@users.noreply.github.com>
2024-03-19 11:49:55 +0800

    Code refactoring

--- rush04.c
+++ rush04.c
@@ -1,47 +1,78 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rush04.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: isgoh <isgoh@student.42kl.edu.my>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/17 10:59:30 by isgoh             #+#    #+#             */
-/*   Updated: 2024/03/18 09:51:30 by isgoh            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-void	ft_putchar(char c);
 
-void	rush(int xW, int yH)
+void ft_putchar(char c);
+
+void ft_print_box(char tl, char tr, char bl, char br, char h, char v, int lines, int cols)
 {
-	int	x;
-	int	y;
-
-	x = 0;
-	y = 1;
-	while ((y <= yH && x <= xW))
+	int i = 0;
+	while(i < lines)
 	{
-		x++;
-		if (x >= xW + 1)
+		int j = 0;
+		while(j < cols)
 		{
-			ft_putchar('\n');
-			x = 0;
-			y = y + 1;
+			// Top corners & horizontal 
+			if(i == 0)
+			{
+				if(j == 0)
+				{
+					ft_putchar(tl);
+				}
+				else if(j == cols - 1)
+				{
+					ft_putchar(tr);
+					ft_putchar('\n');
+				}
+				else
+				{
+					ft_putchar(h);
+				}
+
+			}
+			// Bottom corners & horizontal
+			else if(i == lines - 1)
+			{
+				if(j == 0)
+				{
+					ft_putchar(bl);
+				}
+				else if(j == cols - 1)
+				{
+					ft_putchar(br);
+					ft_putchar('\n');
+				}
+				else
+				{
+					ft_putchar(h);
+				}
+			}
+			// Vertical
+			else
+			{
+				if(j == 0)
+				{
+					ft_putchar(v);
+				}
+				else if(j == cols - 1)
+				{
+					ft_putchar(v);
+					ft_putchar('\n');
+
+				}
+				else
+				{
+					ft_putchar(' ');
+				}
+			}	
+			j++;
 		}
-		else if (x <= 0 || y <= 0)
-			write(1, "use positive lah bro", 20);
-		else if (x == 1 && y == 1)
-			ft_putchar('A');
-		else if ((x == xW && y == 1) || (x == 1 && y == yH))
-			ft_putchar('C');
-		else if (x == xW && y == yH)
-			ft_putchar('A');
-		else if ((x >= 2 || x == xW - 1) && (y == 1 || y == yH))
-			ft_putchar('B');
-		else if ((x == 1 || x == xW) && (y >= 2 || y == yH - 1))
-			ft_putchar('B');
-		else
-			ft_putchar(' ');
+		i++;
 	}
+}
+
+void rush(int x, int y)
+{
+	int lines = y;
+	int cols = x;
+
+	ft_print_box('A', 'C', 'C', 'A', 'B', 'B', lines, cols);
 }
